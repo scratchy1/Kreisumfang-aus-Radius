0667d772fa80315a1e3a6621ed362c3a342c0737
scratchy1 <46693049+scratchy1@users.noreply.github.com>
2019-01-17 04:15:38 +0100

    Add files via upload

---
+++ kreisumfangausradius.pas
@@ -0,0 +1,13 @@
+program kreisumfangausradius (input,output);
+const
+PI=3.141592654;
+var
+radius,umfang:real;
+begin
+writeln('berechnung des kreisumfangs');
+write (' radiuseingabe');
+readln(radius);
+umfang:=2.0*PI*Radius;
+writeln('der umfang  betr„gt:',umfang) ;
+readln;
+end.
